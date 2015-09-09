19827a56f7fee988a555f065a3f2f98389b500ea
Dustin Cass <dustin.cass@gmail.com>
2015-09-08 17:10:22 -0700

    Made mousedown prevention more specific

--- src/combo-breaker.js
+++ src/combo-breaker.js
@@ -175,7 +175,9 @@
                     });
 
                     element.on("mousedown", function (e) {
-                        e.preventDefault();
+                        if (!$(e.target).is(input)) {
+                            e.preventDefault();
+                        }
                     });
 
                     if (scope.strict) {
